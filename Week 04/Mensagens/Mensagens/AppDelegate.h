5d7bbb4e7cf722d6e1d31913f5108a72483af0b1
Teobaldo Mauro de Moura <tmoura@MACCIT013955.local>
2015-09-09 17:59:21 -0300

    Treinamento sobre envio de e-mail

---
+++ Week 04/Mensagens/Mensagens/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mensagens
+//
+//  Created by Teobaldo Mauro de Moura on 9/9/15.
+//  Copyright © 2015 Teobaldo Mauro de Moura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Week 04/Mensagens/Mensagens/ViewController.m
@@ -0,0 +1,65 @@
+//
+//  ViewController.m
+//  Mensagens
+//
+//  Created by Teobaldo Mauro de Moura on 9/9/15.
+//  Copyright © 2015 Teobaldo Mauro de Moura. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <MessageUI/MessageUI.h>
+
+@interface ViewController () <MFMailComposeViewControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIButton *sendEmailButton;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Email disponível ?
+    [[self sendEmailButton] setEnabled:[MFMailComposeViewController canSendMail]];
+}
+
+- (IBAction)sendEmail:(UIButton *)sender {
+
+    MFMailComposeViewController *email = [[MFMailComposeViewController alloc] init];
+    
+    // delegate
+    [email setMailComposeDelegate:self];
+
+    // assunto
+    [email setSubject:@"CI&T - Academia Digital - Teste"];
+    
+    // destinatarios
+    [email setToRecipients:@[ @"teobaldo.bh@hotmail.com" ]];
+    
+    // copia e copia oculta
+    // [email setCcRecipients:[NSArray array]];
+    // [email setBccRecipients:[NSArray array]];
+    
+    // mensagem
+    [email setMessageBody:@"CI&T - Academia Digital - Teste" isHTML:NO];
+    
+    // anexo
+    // [email addAttachmentData:[[NSData alloc] init]
+    //                 mimeType:[NSString string]
+    //                 fileName:[NSString string]];
+    
+    // envia o email
+    [self presentViewController:email animated:YES completion:nil];
+}
+
+#pragma mark - MFMailComposeViewControllerDelegate
+
+-(void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error {
+    NSLog(@"%d - %@", result, error);
+    
+    // Retorna para a primeira tela
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
